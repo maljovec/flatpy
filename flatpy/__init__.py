b8cf854123e4607ce7801f23e0c5b607c80b106c
dmaljovec <daniel.maljovec@recursionpharma.com>
2022-11-13 22:28:23 -0700

    simplify build tooling

--- flatpy/__init__.py
+++ flatpy/__init__.py
@@ -3,4 +3,3 @@ import flatpy.twoD
 import flatpy.utils  # noqa: F401
 
 __all__ = ["twoD", "nD", "utils"]
-__version__ = "0.0.3"
--- setup.py
+++ setup.py
@@ -1,28 +1,7 @@
 """
       Setup script for flatpy
 """
-import re
-
 from setuptools import setup
-
-
-def get_property(prop, project):
-    """
-    Helper function for retrieving properties from a project's
-    __init__.py file
-    @In, prop, string representing the property to be retrieved
-    @In, project, string representing the project from which we will
-    retrieve the property
-    @Out, string, the value of the found property
-    """
-    result = re.search(
-        r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
-        open(project + "/__init__.py").read(),
-    )
-    return result.group(1)
-
-
-VERSION = get_property("__version__", "flatpy")
 
 
 def long_description():
@@ -48,26 +27,9 @@ url = "https://github.com/maljovec/flatpy"
 setup(
     name="flatpy",
     packages=["flatpy"],
-    version=VERSION,
     description="A library for computing samplings in arbitrary dimensions",
     long_description=long_description(),
-    author="Dan Maljovec",
-    author_email="maljovec002@gmail.com",
     license="BSD",
     test_suite="flatpy.tests",
-    url=url,
-    download_url=f"{url}/archive/{VERSION}.tar.gz",
     keywords=[""],
-    # Consult here: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: C++",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Mathematics",
-    ],
-    setup_requires=["scipy", "numpy"],
-    install_requires=["scipy", "numpy"],
-    python_requires=">=3.7, <4",
 )
